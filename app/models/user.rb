494a97ef8a7ecddb40875926be19ff3e384f23b2
David Hu <davidhu314@gmail.com>
2017-01-09 20:47:46 -0800

    create user model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ActiveRecord::Base
+  validates :username, :email, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: "Password cannot be blank" }
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :session_token, presence: true
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.valid_password?(password) ? user : nil
+  end
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def reset_session_token
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def generate_session_token
+    SecureRandom.urlsafe_base64(128)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+end

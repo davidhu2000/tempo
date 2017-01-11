f56fb105b124346236071970555d241ecea808f3
David Hu <davidhu314@gmail.com>
2017-01-11 09:38:57 -0800

    fixed bug where redirect does not happen on splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -6,41 +6,63 @@ const demoUser = {
   password: 'password'
 };
 
-const Splash = ({ login, children }) => (
-  <div>
-    <div className='navbar'>
-      <div className='navbar-logo'>
-        <h1><Link to='/'>Logo</Link></h1>
-      </div>
+class Splash extends React.Component  {
+  constructor(props) {
+    super(props);
+  }
 
-      <div className='navbar-header'>
-        <h1><Link to='/'>Welcome to Tempo</Link></h1>
-      </div>
+  componentDidMount() {
+    this._redirectLoggedIn();
+  }
 
-      <div className='navbar-buttons'>
-        <div className='navbar-button btn-login'>
-          <Link to='/login'>
-            <span>Login</span>
-          </Link>
+  componentDidUpdate() {
+    this._redirectLoggedIn();
+  }
+
+  _redirectLoggedIn() {
+    if(this.props.loggedIn) {
+      this.props.router.replace('/browse');
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='navbar'>
+          <div className='navbar-logo'>
+            <h1><Link to='/'>Logo</Link></h1>
+          </div>
+
+          <div className='navbar-header'>
+            <h1><Link to='/'>Welcome to Tempo</Link></h1>
+          </div>
+
+          <div className='navbar-buttons'>
+            <div className='navbar-button btn-login'>
+              <Link to='/login'>
+                <span>Login</span>
+              </Link>
+            </div>
+            <div className='navbar-button btn-signup'>
+              <Link to='/signup'>
+                <span>Signup</span>
+              </Link>
+            </div>
+            <div className='navbar-button btn-demo'>
+              <a onClick={ () => this.props.login(demoUser) }>
+                <span>Demo</span>
+              </a>
+
+            </div>
+          </div>
         </div>
-        <div className='navbar-button btn-signup'>
-          <Link to='/signup'>
-            <span>Signup</span>
-          </Link>
-        </div>
-        <div className='navbar-button btn-demo'>
-          <a onClick={ () => login(demoUser) }>
-            <span>Demo</span>
-          </a>
 
+        <div className='splash'>
+          { this.props.children }
         </div>
       </div>
-    </div>
-
-    <div className='splash'>
-      { children }
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default withRouter(Splash);

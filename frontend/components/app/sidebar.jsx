5736c96a5a80ee6a23cff7666d56ef16c8e6ff9b
David Hu <davidhu314@gmail.com>
2017-01-11 17:03:24 -0800

    add hover effects to sidebar

--- frontend/components/app/sidebar.jsx
+++ frontend/components/app/sidebar.jsx
@@ -13,19 +13,19 @@ class Sidebar extends React.Component {
           <Link to='/'>Logo</Link>
         </div>
 
+        <div className='sidebar-browse'>
+          <Link to='/search'>
+            <img src='http://res.cloudinary.com/davidhu2000/image/upload/v1484176608/search_icon_czswj4.svg'/>
+            <span>Search</span>
+          </Link>
+
+          <Link to='/browse'>
+            <img src='http://res.cloudinary.com/davidhu2000/image/upload/v1484176608/browse_icon_b1vtyu.svg'/>
+            <span>Browse</span>
+          </Link>
+        </div>
+
         <div className='sidebar-menu'>
-          <div className='sidebar-browse'>
-            <Link to='/search'>
-              <img src='http://res.cloudinary.com/davidhu2000/image/upload/v1484176608/search_icon_czswj4.svg'/>
-              <span>Search</span>
-            </Link>
-
-            <Link to='/browse'>
-              <img src='http://res.cloudinary.com/davidhu2000/image/upload/v1484176608/browse_icon_b1vtyu.svg'/>
-              <span>Browse</span>
-            </Link>
-          </div>
-
           <div className='sidebar-profile'>
             <Link to='/'>
               <img src='https://image.shutterstock.com/display_pic_with_logo/1142849/149083895/stock-vector-male-avatar-profile-picture-vector-149083895.jpg'/>

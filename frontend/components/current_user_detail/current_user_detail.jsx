a17229ade17aae0a72dd485bb3c20194a8195490
David Hu <davidhu314@gmail.com>
2017-01-16 12:48:20 -0800

    add custom greeting to user profile

--- frontend/components/current_user_detail/current_user_detail.jsx
+++ frontend/components/current_user_detail/current_user_detail.jsx
@@ -5,10 +5,39 @@ class CurrentUserDetail extends React.Component {
     super(props);
   }
 
+  displayUserName() {
+    let user = this.props.currentUser;
+    if(user.first_name === '') {
+      return user.username;
+    } else {
+      return user.first_name;
+    }
+  }
+
+  renderGreeting() {
+    let time = new Date;
+    let hour = time.getHours();
+    if( hour >= 0 && hour < 12 ) {
+      return 'Good morning, ';
+    } else if(hour >= 12 && hour < 18) {
+      return 'Good afternoon, ';
+    } else {
+      return 'Good evening, ';
+    }
+  }
+
   render() {
     return (
       <div className='current-user-profile'>
-        <h1>Hello</h1>
+        <div className='current-user-info'>
+          <div className='current-user-image'>
+            <img src={ this.props.currentUser.image_url} />
+          </div>
+          <div className='current-user-greeting'>
+            { this.renderGreeting() }
+            <span>{ this.displayUserName() }</span>
+          </div>
+        </div>
 
       </div>
     );

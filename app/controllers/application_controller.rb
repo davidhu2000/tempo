7dbd0ccdb787acbb37c102155957b0ad85ca01a9
David Hu <davidhu314@gmail.com>
2017-03-02 19:17:01 -0800

    commented out redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,11 +29,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def redirect_all
-    url = logged_in? ? "/#/browse" : "/#/splash"
-    redirect_to url unless params['controller'] == 'static_pages'
-  end
-
-  before_action :redirect_all
+  # def redirect_all
+  #   url = logged_in? ? "/#/browse" : "/#/splash"
+  #   redirect_to url unless params['controller'] == 'static_pages'
+  # end
+  #
+  # before_action :redirect_all
 
 end

a51fa40cbe238edb3f811dd257aed200ca23bbaf
David Hu <davidhu314@gmail.com>
2017-03-21 10:40:54 -0700

    add import ReactDOM into playlist forms

--- frontend/components/pick_playlist_form/pick_playlist_form.jsx
+++ frontend/components/pick_playlist_form/pick_playlist_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
 class PickPlaylistForm extends React.Component {
   constructor(props) {
--- frontend/components/playlist_form/playlist_form.jsx
+++ frontend/components/playlist_form/playlist_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class PlaylistForm extends React.Component {
   constructor(props) {

eef76c5622180d09b365d2156966ba9d2a8de2a5
David Hu <davidhu314@gmail.com>
2017-03-22 10:58:53 -0700

    update entry file to remove conditional

--- frontend/tempo.jsx
+++ frontend/tempo.jsx
@@ -7,17 +7,15 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
 
-  let store;
-  if(window.currentUser) {
-    const preloadedState = {
-      session: {
-        currentUser: window.currentUser
-      }
-    };
-    store = configureStore(preloadedState);
-  } else {
-    store = configureStore();
-  }
+   const preloadedState = {
+     session: {
+       currentUser: window.currentUser || null
+     }
+   };
+
+  let store = configureStore(preloadedState);
+
+  window.store = store;
 
   ReactDOM.render(<Root store={ store } />, root);
 });
